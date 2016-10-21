32576171222801f94e7fe4fa47b02cf1165f2415
somename <someemail>
2016-10-21 16:41:06 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -1,7 +1,7 @@
 
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
 // ID:145
-// Finish time:
+// Finish time: 16:40
 
 public class PlanetExplorer {
 	
